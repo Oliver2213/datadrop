8fb9af8456fde4b85853ddec07374c72a650be2e
Blake Oliver <oliver22213@me.com>
2017-09-24 01:37:28 -0400

    Add a basic API route function for creating drops
    I'll probably split off the last bit of code from that function into one that is for showing a specific drop (though with an option to take out the drop keys).

--- app/views/__init__.py
+++ app/views/__init__.py
@@ -2,3 +2,4 @@
 
 from index import index
 from drop import show_drop, show_raw_drop
+import api
---
+++ app/views/api.py
@@ -0,0 +1,52 @@
+# API view functions
+
+from collections import OrderedDict
+from flask import jsonify, url_for, redirect, request
+
+from app import app, db, models
+
+
+@app.route('/api/drop/create', methods=['POST'])
+def create_drop():
+	"""API view function to create a new drop."""
+	if request.form.get('data') == None:
+		return jsonify({"error": "You must provide some data when creating a new drop."}), 400
+	drop = models.Drop()
+	title = request.form.get('title')
+	if title != None and title != '':
+		drop.title = title
+	drop.data = request.form.get('data')
+	publicly_listed = request.form.get('publicly_listed', app.config['DATADROP_DEFAULT_LISTED'])
+	drop.publicly_listed = publicly_listed
+	expires = request.form.get('expires', app.config['DATADROP_DEFAULT_EXPIRES'])
+	drop.expires = expires
+	expires_in = request.form.get('expires_in', app.config['DATADROP_DEFAULT_EXPIRES_IN'])
+	drop.expires_in = expires_in
+	self_destructs = request.form.get('self_destructs', app.config['DATADROP_DEFAULT_SELF_DESTRUCTS'])
+	drop.self_destructs = self_destructs
+	drop_key_strings = request.form.getlist('drop_keys')
+	if drop_key_strings != None and drop_key_strings != []:
+		# for each drop key that was provided, check if it exists in the database;
+		# if it doesn't, create it
+		drop_keys = [] # a list of key instances
+		for k in drop_key_strings:
+			potential_key = db.DropKey.filter_by(key=k.lower()).one_or_none()
+			if potential_key == None:
+				potential_key = db.save(models.DropKey(key=k.lower()))
+			drop_keys.append(potential_key)
+		drop.drop_keys = drop_keys
+		db.save(drop)
+		# now build the response
+		r = OrderedDict()
+		if drop.title:
+			r['title'] = drop.title
+		r['url'] = url_for('show_drop', urlstring=drop.urlstring, _external=True)
+		r['created_at'] = drop.created_at
+		r['publicly_listed'] = drop.publicly_listed
+		r['expires'] = drop.expires
+		if drop.expires:
+			r['expires_in'] = drop.expires_in
+		r['self_destructs'] = drop.self_destructs
+		if drop.drop_keys:
+			r['drop_keys'] = [k.key for k in drop.drop_keys]
+		return jsonify(r), 201
