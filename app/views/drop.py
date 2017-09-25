32803564ffb745f40a26a0fe670915d8bd4afadc
Blake Oliver <oliver22213@me.com>
2017-09-25 07:02:15 -0400

    Update the show_drop and show_raw_drop views so they properly count views and delete drops which are supposed to self destruct

--- app/views/drop.py
+++ app/views/drop.py
@@ -12,6 +12,12 @@ def show_drop(urlstring):
 		urlstring = urlstring[:-1]
 	drop = db.Drop.filter_by(urlstring=urlstring).one_or_none()
 	if drop != None: # a drop with the provided urlstring exists
+		drop.views+=1
+		if drop.self_destructs and drop.views >= drop.self_destructs_in:
+			db.delete(drop)
+			db.commit()
+		else:
+			db.save(drop)
 		return render_template('drop.html', drop=drop)
 	else: # no drop
 		return render_template('drop_not_found.html'), 404
@@ -21,6 +27,12 @@ def show_raw_drop(urlstring):
 	"""Return the drop with the given urlstring (if it exists); this is different from show_drop because it doesn't put the normal datadrop web things around it (headings, navigation, etc)."""
 	drop = db.Drop.filter_by(urlstring=urlstring).one_or_none()
 	if drop != None:
+		drop.views += 1
+		if drop.self_destructs and drop.views >= drop.self_destructs_in:
+			db.delete(drop)
+			db.commit()
+		else:
+			db.save(drop)
 		# explicitly create the response object here, because the mimetype needs to be text/plain
 		resp = Response(drop.data, mimetype='text/plain')
 		return resp
