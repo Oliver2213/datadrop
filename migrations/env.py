b874b30f9970fe059af0118cc4b6475b4f5e5f63
Blake Oliver <oliver22213@me.com>
2017-09-25 06:37:28 -0400

    Make the alembic environment properly import the database model class and set it so it uses whatever database URL is set by the flask app's configuration

--- migrations/env.py
+++ migrations/env.py
@@ -2,6 +2,8 @@ from __future__ import with_statement
 from alembic import context
 from sqlalchemy import engine_from_config, pool
 from logging.config import fileConfig
+import os
+import sys
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -13,8 +15,14 @@ fileConfig(config.config_file_name)
 
 # add your model's MetaData object here
 # for 'autogenerate' support
+sys.path.append(os.getcwd())
 from app.models import Model
+from app import app
+
 target_metadata = Model.metadata
+
+# get the database URL from the configuration object on the flask app
+config.set_main_option('sqlalchemy.url', app.config['SQL_DATABASE_URI'])
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
